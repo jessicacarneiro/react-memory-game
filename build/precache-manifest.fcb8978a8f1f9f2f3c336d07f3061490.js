d39c1dceaffa1eadf3120d676eceb1a2d139d179
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2020-10-30 09:27:52 -0300

    Deploying to Heroku

---
+++ build/precache-manifest.fcb8978a8f1f9f2f3c336d07f3061490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a10ec9191de0e1c8aee036a58e4d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dff77e5dba28129a721f",
+    "url": "/static/css/main.f1ffeeb2.chunk.css"
+  },
+  {
+    "revision": "85dbdf1fc7aff37342e4",
+    "url": "/static/js/2.07f7b235.chunk.js"
+  },
+  {
+    "revision": "e5067c2807f8c6eeca3663552efd09fb",
+    "url": "/static/js/2.07f7b235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff77e5dba28129a721f",
+    "url": "/static/js/main.f3d785e5.chunk.js"
+  },
+  {
+    "revision": "98a230f2312dc5e75d35",
+    "url": "/static/js/runtime-main.f0b3a8ce.js"
+  }
+]);
